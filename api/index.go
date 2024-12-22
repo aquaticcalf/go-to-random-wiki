ce8dc8145e43f57109b0cbf27de9f75e263779ac
sam <aquaticcalf@proton.me>
2024-12-22 22:18:05 +0530

    meow

--- api/index.go
+++ api/index.go
@@ -7,5 +7,3 @@ import (
 func Handler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "https://en.wikipedia.org/wiki/Special:Random", http.StatusFound)
 }
-
-var Handlerfunc = http.HandlerFunc(Handler)
