19a414f2ce9b6c187e16358317e74363322c1c97
sam <aquaticcalf@proton.me>
2024-12-22 22:32:57 +0530

    meow

--- api/index.go
+++ api/index.go
@@ -9,7 +9,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	server := New()
 
 	server.GET("/*", func(context *Context) {
-		http.Redirect(context.Writer, context.Request, "https://en.wikipedia.org/wiki/Special:Random", http.StatusFound)
+		context.Redirect(context.Writer, context.Request, "https://en.wikipedia.org/wiki/Special:Random", http.StatusFound)
 	})
 
 	server.Handle(w, r)
