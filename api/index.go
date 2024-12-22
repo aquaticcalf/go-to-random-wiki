e90908ab91cfbc92c2467a279224bbe700a153ad
sam <aquaticcalf@proton.me>
2024-12-22 22:37:05 +0530

    meow

--- api/index.go
+++ api/index.go
@@ -1,17 +1,14 @@
 package handler
 
 import (
-	"net/http"
-	. "github.com/tbxark/g4vercel"
+    "net/http"
+    . "github.com/tbxark/g4vercel"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	server := New()
-
-	server.GET("/*", func(context *Context) {
-		context.Redirect(context.Writer, context.Request, "https://en.wikipedia.org/wiki/Special:Random", http.StatusFound)
-	})
-
-	server.Handle(w, r)
+    server := New()
+    server.GET("/*", func(context *Context) {
+        http.Redirect(context.Writer, r, "https://en.wikipedia.org/wiki/Special:Random", http.StatusMovedPermanently)
+    })
+    server.Handle(w, r)
 }
-
